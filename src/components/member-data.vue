ed19bcf5096b27abbeaeb7b4955f748a742ee70e
alrwest <105620482+alrwest@users.noreply.github.com>
2023-11-30 01:33:54 -0500

    updating src

---
+++ src/components/member-data.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <h2> {{ name }}</h2>
+        <h3> {{ role }}</h3>
+        <button> Show/Hide Details </button>
+        <button> Join</button>
+        <div>
+            <p> {{ phone }} </p>
+            <p> {{ email }} </p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }, // props:['name', 'role', 'phone','email']
+    props: {
+        name: {
+            type: String,
+            required: true
+        },
+        role: {
+            type: String,
+            required: true
+        },
+        phone: String,
+        email: String,
+    }
+}
+
+</script>
---
+++ src/components/team-form.vue
@@ -0,0 +1,43 @@
+<template>
+    <h2> Team Form </h2>
+    <div>
+        <!-- getting input for team name-->
+        <input v-model="teamName" type="text" />
+
+        <!-- getting role input -->
+        <input type="checkbox" id="developer" value="developer" v-model="teamRole">
+        <label for="developer">Developer</label>
+
+        <input type="checkbox" id="designer" value="designer" v-model="teamRole">
+        <label for="designer">Designer</label>
+
+        <input type="checkbox" id="UXUI" value="UXUI" v-model="teamRole">
+        <label for="UXUI">UX/UI</label>
+        <button @click="saveTeamName"> Submit </button>
+    </div>
+    
+
+</template>
+
+<script>
+
+export default {
+    data() { 
+        return { 
+            teamName: ''
+        }
+    },
+    emits:['team-form'],
+    methods: {
+        submitData() {
+            console.log("submit data clicked");
+            this.$emit('team-form', this.teamName);
+        },
+        saveTeamName() {
+            console.log("Team Name: ", teamName);
+
+        }
+    }
+}
+
+</script>

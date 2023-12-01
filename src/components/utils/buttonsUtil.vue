9136c7132c32ce7be96eb144b83d3dcc09c61105
alrwest <105620482+alrwest@users.noreply.github.com>
2023-12-01 05:18:45 -0500

    updating src...

---
+++ src/components/utils/buttonsUtil.vue
@@ -0,0 +1,25 @@
+<template>
+   <button class="buttons">
+        <slot> Button Util </slot>
+   </button>
+</template>
+
+<style>
+    .buttons {
+        width: 150px;
+        height: 30px;
+        border: 1px solid #4E5179;
+        border-radius: 10px;
+        background-color: #F7D1B7;
+        color: #4E5179;
+        cursor: pointer;
+    }
+
+    .buttons:hover {
+        color: #fff;
+        background-color: #4E5179;
+
+    }
+
+
+</style>
